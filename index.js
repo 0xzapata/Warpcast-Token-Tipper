11691acb84b45cbfd60ccf7f02ec081ad039428c
Niel <75532567+0xzapata@users.noreply.github.com>
2024-03-01 16:50:07 +0800

    added comment docs in index.js

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import yargs from 'yargs';
 import tipReplies from "./tipRepliesUsingCsv.js"
 import saveReplies from "./saveRepliesToCsv.js"
 
+// Parse command line arguments
 const argv = yargs(process.argv.slice(2))
+    // Define the 'SAVE_REPLIES' command
     .command('SAVE_REPLIES', 'Save replies', {
         url: {
             description: 'The URL to save replies to',
@@ -16,6 +18,7 @@ const argv = yargs(process.argv.slice(2))
             default: undefined
         }
     })
+    // Define the 'TIP_REPLIES' command
     .command('TIP_REPLIES', 'Tip replies', {
         amount: {
             description: 'The amount of token to send to each recipient',
@@ -35,17 +38,20 @@ const argv = yargs(process.argv.slice(2))
             default: undefined
         }
     })
+    // Display help message
     .help()
     .argv;
 
+// Get the command from the arguments and convert it to uppercase
 const command = argv._[0].toUpperCase();
 
+// Execute the appropriate function based on the command
 if (command === 'SAVE_REPLIES') {
     saveReplies(argv.url, argv.outputFilename);
-
 } else if (command === 'TIP_REPLIES') {
     tipReplies(argv.amount, argv.csvFile, argv.sentReceipts);
 } else {
+    // If the command is not recognized, display an error message and exit
     console.error('Invalid argument. Please provide either SAVE_REPLIES or TIP_REPLIES');
     process.exit(1);
 }
